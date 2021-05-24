499e266a1b0d8eea64d791d41476ae9e352449e4
bachvu <bachvu@maysoft.vn>
2021-05-24 16:20:36 +0700

    asd

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
-console.log("bachvu")
-console.log("minhnguyen")
+$(function () {
+
+    // Only show submit button on proper email
+    (function ($, window, document, undefined) {
+        'use strict';
+        var form = '#wf-form-Email-Form',
+            className = 'email-active',
+            submit = 'input[type="submit"]',
+            email = 'input[type="email"]';
+        $(form).each(function () {
+            var $form = $(this),
+                $email = $form.find(email),
+                $submit = $form.find(submit),
+                val = '';
+            $email.on('keyup.addClassWhenEmail', function () {
+                val = $email.val();
+                $submit.toggleClass(className, val != '' && /^([\w-\.]+@([\w-]+\.)+[\w-]{2, 12})?$/.test(val));
+            });
+        });
+    })(jQuery, window, document);
+})
